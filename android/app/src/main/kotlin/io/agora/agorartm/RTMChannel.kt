b5e6c740d9949be633f3ad231eb48c4582762a8d
hanaa <ahkamkhalaf@gmail.com>
2020-03-16 17:06:16 +0200

    app construct

---
+++ android/app/src/main/kotlin/io/agora/agorartm/RTMChannel.kt
@@ -0,0 +1,88 @@
+package io.agora.agorartm
+
+import android.os.Handler
+import io.agora.rtm.RtmChannelAttribute
+import io.agora.rtm.RtmChannelListener
+import io.agora.rtm.RtmChannelMember
+import io.agora.rtm.RtmMessage
+import io.flutter.plugin.common.BinaryMessenger
+import io.flutter.plugin.common.EventChannel
+
+class RTMChannel : RtmChannelListener, EventChannel.StreamHandler {
+
+    val messenger: BinaryMessenger
+    val eventChannel: EventChannel
+    val clientIndex: Long
+    val eventHandler: Handler
+
+    var eventSink: EventChannel.EventSink?
+
+
+    constructor(clientIndex: Long, channelId: String, messenger: BinaryMessenger, eventHandler: Handler) {
+        this.clientIndex = clientIndex
+        this.messenger = messenger
+        this.eventChannel = EventChannel(this.messenger, "io.agora.rtm.client${clientIndex}.channel${channelId}")
+        this.eventChannel.setStreamHandler(this)
+        this.eventSink = null
+        this.eventHandler = eventHandler
+    }
+
+    private fun runMainThread(f: () -> Unit) {
+        eventHandler.post(f)
+    }
+
+    override fun onAttributesUpdated(p0: MutableList<RtmChannelAttribute>?) {
+        
+    }
+
+    override
+    fun onMessageReceived(message: RtmMessage, member: RtmChannelMember) {
+        sendChannelEvent("onMessageReceived", hashMapOf(
+                "userId" to member.userId,
+                "channelId" to member.channelId,
+                "message" to hashMapOf(
+                        "text" to message.text,
+                        "offline" to message.isOfflineMessage,
+                        "ts" to message.serverReceivedTs
+                )
+        ))
+    }
+
+    override
+    fun onMemberJoined(member: RtmChannelMember) {
+        sendChannelEvent("onMemberJoined", hashMapOf(
+                "userId" to member.userId,
+                "channelId" to member.channelId
+        ))
+    }
+
+    override
+    fun onMemberLeft(member: RtmChannelMember) {
+        sendChannelEvent("onMemberLeft", hashMapOf(
+                "userId" to member.userId,
+                "channelId" to member.channelId
+        ))
+    }
+
+    override fun onMemberCountUpdated(p0: Int) {
+
+    }
+
+    private fun sendChannelEvent(eventName: String, params: HashMap<Any, Any>) {
+        var map = params.toMutableMap()
+        map["event"] = eventName
+        runMainThread {
+            this.eventSink?.success(map)
+        }
+    }
+
+    override
+    fun onListen(params: Any?, eventSink: EventChannel.EventSink) {
+        this.eventSink = eventSink
+    }
+
+    override
+    fun onCancel(params: Any?) {
+        this.eventSink = null
+    }
+}
---
+++ lib/messaging/agora_rtm.dart
@@ -0,0 +1,3 @@
+export 'classes/agora_rtm_channel.dart';
+export 'classes/agora_rtm_client.dart';
+export 'classes/utils.dart';
